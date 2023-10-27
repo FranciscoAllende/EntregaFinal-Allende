f1d7a9d9a3bc4797c0a86c1185a3467385dbd763
Francisco Allende <f.allende020@gmail.com>
2023-10-26 21:54:32 -0300

    Añadí todos los requisitos para la tercera preentrega, JSON, DOM, Local Storage y borré los prompts y alerts

--- js/script.js
+++ js/script.js
@@ -1,93 +1,79 @@
-// - - - Usuario - - - //
-let nombreUsuario = prompt('Bienvenidx a CoderFitness. Ingrese su nombre.');
-
 // - - - Productos y precios - - - //
-let productos = [
-   {
-        nombre: 'Proteína Star',
-        precio: 33000,
-    },
-    {
-        nombre: 'Creatina Star',
-        precio: 17000,
-    },
-    {
-        nombre: 'Preentreno C4',
-        precio: 60000,
-    },
-    {
-        nombre: 'Mass Gainer',
-        precio: 15000,
-    },
+const productos = [{
+    id: 1,
+    product: 'Proteína Star',
+    thumbnail: './images/productouno.png',
+    price: '33000',
+},
+{
+    id: 2,
+    product: 'Creatina Star',
+    thumbnail: './images/productodos.png',
+    price: '17000',
+},
+{
+    id: 3,
+    product: 'Preentreno C4',
+    thumbnail: './images/productotres.png',
+    price: '60000',
+},
+{
+    id: 4,
+    product: 'Mass Gainer',
+    thumbnail: './images/productocuatro.png',
+    price: '15000',
+},
 ];
-// - - - Variables financieras - - - //
-let descuentoEfectivo = 0.80;
-let descuentoTarjeta = 0.90;
-let descuentoMercadoPago = 0.85;
 
-const calcularDescuento = (precio, descuento) => {
-    return precio * descuento;
-};
+const form = document.querySelector('#form');
+const formContainer = document.querySelector('.form-container-inputs');
 
-let productosAdquiridos = [];
 
-while (true) {
-    let opciones = 'Bienvenidx, ' + nombreUsuario + '. ¿Cuál de nuestros productos te interesa?\n';
+formContainer.innerHTML = `
+    <div>
+        <label for="name">Nombre:</label>
+        <input id="name" placeholder="Ingrese su nombre" type="text">
+    </div>
+    <div>
+        <label for="email">Email:</label>
+        <input id="email" placeholder="Ingrese su email" type="email">
+    </div>
+    <button id="save-button">¡Enviar!</button>
+    `;
 
-    for (let i = 0; i < productos.length; i++) {
-        opciones += (i + 1) + ' - ' + productos[i].nombre + ' - U$S ' + productos[i].precio + '\n';
-    }
+form.appendChild(formContainer);
 
-    opciones += (productos.length + 1) + ' - Finalizar selección de productos';
-    let opcionProducto = prompt(opciones);
+const saveData = () => {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
 
-    if (opcionProducto >= 1 && opcionProducto <= productos.length) {
-        let productoElegido = productos[opcionProducto - 1];
-        let metodoPago;
-
-        do {
-            let mensajeProducto = 'Elegiste ' + productoElegido.nombre + ' y su precio es U$S ' + productoElegido.precio;
-            mensajeProducto += '\nElige un método de pago.\n1 - Efectivo (20% OFF)\n2 - Mercado Pago (15% OFF)\n3 - Tarjeta Débito/Crédito (10% OFF)\n4 - Volver al menú principal';
-            metodoPago = prompt(mensajeProducto);
-
-            switch (metodoPago) {
-                case '1':
-                    let precioFinalEfectivo = calcularDescuento(productoElegido.precio, descuentoEfectivo).toFixed(2);
-                    alert('Eligió pagar con efectivo. El precio final es U$S ' + precioFinalEfectivo);
-                    alert('¡Felicitaciones! Se realizó correctamente la compra de tu producto: ' + productoElegido.nombre);
-                    productosAdquiridos.push(productoElegido);
-                    break;
-                case '2':
-                    let precioFinalMercadoPago = calcularDescuento(productoElegido.precio, descuentoMercadoPago).toFixed(2);
-                    alert('Elegió pagar con Mercado Pago. El precio final es U$S ' + precioFinalMercadoPago);
-                    alert('¡Felicitaciones! Se realizó correctamente la compra de tu producto: ' + productoElegido.nombre);
-                    productosAdquiridos.push(productoElegido);
-                    break;
-                case '3':
-                    let precioFinalTarjeta = calcularDescuento(productoElegido.precio, descuentoTarjeta).toFixed(2);
-                    alert('Eligió pagar con tarjeta débito/crédito. El precio final es U$S ' + precioFinalTarjeta);
-                    alert('¡Felicitaciones! Se realizó correctamente la compra de tu producto: ' + productoElegido.nombre);
-                    productosAdquiridos.push(productoElegido);
-                    break;
-                case '4':
-                    break;
-                default:
-                    alert('Opción no válida. Por favor seleccioná una opción válida.');
-            }
-        } while (metodoPago !== '4');
-    } else if (opcionProducto == productos.length + 1) {
-        break;
-    } else {
-        alert('Opción no válida. Por favor, seleccioná una opción válida.');
+    if (name && email) {
+        const data = {
+            name,
+            email,
+        }
+        localStorage.setItem('userData', JSON.stringify(data));
+        console.log(data)
     }
 }
 
-if (productosAdquiridos.length > 0) {
-    let mensajeProductosAdquiridos = 'Productos adquiridos por ' + nombreUsuario + ':\n';
-    for (let i = 0; i < productosAdquiridos.length; i++) {
-        mensajeProductosAdquiridos += productosAdquiridos[i].nombre + '\n';
-    }
-    alert(mensajeProductosAdquiridos);
-} else {
-    alert('No compró ningún producto en esta sesión.');
+const saveButton = document.querySelector('#save-button');
+    saveButton.addEventListener('click', saveData)
+
+
+
+const mainElement = document.querySelector('main');
+
+let cards = document.createElement('div');
+
+for (const articuloVenta of productos) {
+    cards.innerHTML += ` <article class="card" id="${articuloVenta.id}">
+                        <img src="${articuloVenta.thumbnail}" alt="">
+                        <div class="card-body">
+                            <p>${articuloVenta.product}</p>
+                            <p>U$S ${articuloVenta.price}</p>
+                        </div>
+                    </article>`;
 }
+
+mainElement.appendChild(cards);
